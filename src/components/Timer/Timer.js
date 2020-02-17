18646b2acd38dc223dfcdc7c87b56cdba95cba4a
Radhika Murugesan <radhika.murugesan@perficient.com>
2020-02-17 10:46:34 +0530

    Timer code updated

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -4,90 +4,110 @@ export default class Timer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        timerA: 0,   
-        timerB: 0,        
-        checked: false,        
-        maxTimeCount: 50,
-        start: 0
-    }
+      timerA: 0,
+      timerB: 0,
+      checked: true,
+      maxTimeCount: 50
+    };
+    this.timerASeconds = 1000;
+    this.timerBSeconds = 2000;
   }
-  async componentDidMount() {
-     this.timerA();
-     this.timerB();
-
-      
-  }
-
-  timerA = () => {
-    this.setState({          
-      timerA: this.state.timerA,
-    })
-    this.timer = setInterval(() => this.setState({
-      timerA: this.state.timerA + 1
-    }), 1000);
+  componentDidMount() {
+    this.initTimerA();
+    this.initTimerB();
   }
 
+  initTimerA = () => {
+    clearInterval(this.timerA);
+    this.timerA = setInterval(
+      () => {
+        if (this.state.timerA < this.state.maxTimeCount) {
+          this.setState({
+            timerA: this.state.timerA + 1
+          });
+        } else {
+          clearInterval(this.timerA);
+        }
+      },
 
-  timerB= () => {
-    this.setState({          
-      timerB: this.state.timerB,
-    })
-    this.timer = setInterval(() => this.setState({
-      timerB: this.state.timerB + 1
-    }), 1500);
-  }
-
-  startTimer = (event) => {
-      event.preventDefault();  
-      console.log('start');
-      
-        let intervalsA = setInterval(() => {   
-          while(this.state.timerA>=this.state.maxTimeCount) {
-            this.setState({
-              timerA: this.state.timerA +1
-          }) 
-          }
-                 
-        }, 1000);
-
-        clearInterval(intervalsA);
-      
-        let intervalsB = setInterval(() => {   
-          while(this.state.timerB>=this.state.maxTimeCount) {
-            this.setState({
-              timerB: this.state.timerB +1
-          }) 
-          }       
-        }, 2000);
-     
-        clearInterval(intervalsB);
+      this.timerASeconds
+    );
   };
 
-  resetTimer = (event) => {
-    event.preventDefault();      
-       this.setState({
-           timerA: 0,
-           timerB: 0
-       })
-      
+  initTimerB = () => {
+    clearInterval(this.timerB);
+    this.timerB = setInterval(
+      () => {
+        if (this.state.timerB < this.state.maxTimeCount) {
+          this.setState({
+            timerB: this.state.timerB + 1
+          });
+        } else {
+          clearInterval(this.timerB);
+        }
+      },
+
+      this.timerBSeconds
+    );
+  };
+
+  check = event => {
+    console.log(event.target.checked);
+
+    this.setState({
+      checked: !this.state.checked
+    });
+    if (event.target.checked) {
+      this.timerASeconds = 1000;
+      this.timerBSeconds = 2000;
+    } else {
+      console.log("else clibk");
+
+      this.timerASeconds = 2000;
+      this.timerBSeconds = 1000;
+    }
+    this.initTimerA();
+    this.initTimerB();
+  };
+
+  startTimer = event => {
+    this.initTimerA();
+    this.initTimerB();
+  };
+
+  resetTimer = event => {
+    event.preventDefault();
+    clearInterval(this.timerA);
+    clearInterval(this.timerB);
+    this.setState({
+      timerA: 0,
+      timerB: 0
+    });
   };
 
   render() {
     return (
       <div>
         <h3>Timer</h3>
-        <div>            
-            <input type="text" value = {this.state.timerA} /> 
-           <input type="text" value = {this.state.timerB} />
-          <input type="checkbox" checked={this.state.checked} />
+        <div>
+          <input type="text" value={this.state.timerA} />
+          <input type="text" value={this.state.timerB} />
+          <input
+            type="checkbox"
+            checked={this.state.checked}
+            onChange={this.check}
+          />
         </div>
         <br />
-        <input type="text" value = {this.state.maxTimeCount}/>
+        <input type="text" value={this.state.maxTimeCount} />
         <br />
         <div>
           <button onClick={this.startTimer}>Start</button>
           <button onClick={this.resetTimer}>Reset</button>
-          
+          {this.state.timerA === this.state.maxTimeCount &&
+          this.state.timerB === this.state.maxTimeCount ? (
+            <p>Maximum Count Reached</p>
+          ) : null}
         </div>
       </div>
     );
